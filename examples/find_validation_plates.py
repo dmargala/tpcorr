11e30851483e2b162b64f6e4d68cf72c6052f08a
Daniel Margala <djmargala@gmail.com>
2016-01-28 12:55:17 -0800

    more parens

--- examples/find_validation_plates.py
+++ examples/find_validation_plates.py
@@ -36,11 +36,11 @@ def main():
 
     sample_names = ['Offset targets', 'Quasars', 'Failed quasars', 'Spec. standards', 'Offset standards']
     sample_selections = [
-        'LAMBDA_EFF=4000 and (ZWARNING&1<<7)=0',
-        'LAMBDA_EFF=4000 and (ZWARNING&1<<7)=0 and OBJTYPE="QSO" and CLASS="QSO"',
-        'LAMBDA_EFF=4000 and (ZWARNING&1<<7)=0 and OBJTYPE="QSO" and CLASS="STAR"',
-        'LAMBDA_EFF=5400 and (ZWARNING&1<<7)=0 and OBJTYPE="SPECTROPHOTO_STD" and CLASS="STAR"',
-        'LAMBDA_EFF=4000 and (ZWARNING&1<<7)=0 and CLASS="STAR" and ANCILLARY_TARGET2=(1<<20)',
+        'LAMBDA_EFF=4000 and (ZWARNING&(1<<7))=0',
+        'LAMBDA_EFF=4000 and (ZWARNING&(1<<7))=0 and OBJTYPE="QSO" and CLASS="QSO"',
+        'LAMBDA_EFF=4000 and (ZWARNING&(1<<7))=0 and OBJTYPE="QSO" and CLASS="STAR"',
+        'LAMBDA_EFF=5400 and (ZWARNING&(1<<7))=0 and OBJTYPE="SPECTROPHOTO_STD" and CLASS="STAR"',
+        'LAMBDA_EFF=4000 and (ZWARNING&(1<<7))=0 and CLASS="STAR" and ANCILLARY_TARGET2=(1<<20)',
     ]
 
     for name, selection in zip(sample_names, sample_selections):
@@ -48,11 +48,17 @@ def main():
 
     print
 
+    dr12b_sql_postfix = 'CHUNK not in ({})'.format(bad_chunks_str)
+    valid_sql_postfix = 'ZWARNING=0 and PLATE in ({})'.format(validiation_plates_str)
+
+    print 'DR12b: {}'.format(dr12b_sql_postfix)
+    print 'validation: {}'.format(valid_sql_postfix)
+
     for name, selection in zip(sample_names, sample_selections):
         sql_prefix = 'SELECT {} FROM meta'.format(what)
         dr12_sql = sql_prefix + ' WHERE {}'.format(selection)
-        dr12b_sql = sql_prefix + ' WHERE {} and CHUNK not in ({})'.format(selection, bad_chunks_str)
-        valid_sql = sql_prefix + ' WHERE {} and ZWARNING=0 and PLATE in ({})'.format(selection, validiation_plates_str)
+        dr12b_sql = sql_prefix + ' WHERE {} and {}'.format(selection, dr12b_sql_postfix)
+        valid_sql = sql_prefix + ' WHERE {} and {}'.format(selection, valid_sql_postfix)
 
         meta_db.cursor.execute(dr12_sql)
         rows = meta_db.cursor.fetchall()
